c66946e38348c98a367d979465255d618cb4dd74
Maximalic <maxime.duverger@student.junia.com>
2023-03-21 14:46:40 +0100

    Ajout numérotation coté
    Ajout de la numérotation sur le coté du board ainsi que déplacement de la génération du board dans le back

--- front/display.js
+++ front/display.js
@@ -12,7 +12,50 @@ const chessPieceInit = [
 	"black-rook", "black-knight", "black-bishop", "black-queen", "black-king", "black-bishop", "black-knight", "black-rook"  // Black pieces
 ];
 
+
+let board = document.getElementById("board");
+for (let i = 0; i < 9; i++) {
+	let row = document.createElement("div");
+	row.className = "row";
+	
+		for (let j = 0; j < 9; j++) {
+			let cell = document.createElement("div");
+			if(i==0){
+				cell.className = "colonne";
+				cell.innerText = String.fromCharCode(96 + j);
+				if(j==0){
+					cell.className = "ligne";
+				}
+			}
+			else if(j==0){
+				cell.className = "ligne";
+				cell.innerText = i;
+			}
+			else{
+				cell.className = "square";
+				cell.id = String.fromCharCode(96 + j) + (i);
+				if((j+i)%2==1){
+					cell.className += " light";
+				}
+				else{
+					cell.className += " dark";
+				} 
+			}
+			row.appendChild(cell);
+		}
+	board.appendChild(row);
+}
+
+
 //chess pieces initialisation
 for (let i = 0; i < chessCoordInit.length; i++) {
-	document.getElementById(chessCoordInit[i]).innerHTML = `<img src="public/pieces/${chessPieceInit[i]}.png" alt="${chessPieceInit[i]}" class="piece" id="${chessPieceInit[i]}">`;
+	document.getElementById(chessCoordInit[i]).innerHTML = `<img src="public/pieces/${chessPieceInit[i]}.png" onclick="SeeInfo('${chessPieceInit[i]+i}')" alt="${chessPieceInit[i]}" class="piece" id="${chessPieceInit[i]+i}">`;
+}
+
+
+function SeeInfo(piece){
+	//get the id of the div just above the piece
+	let div = document.getElementById(piece).parentNode.id;
+	//get the id of the piece
+	console.log(div)
 }
