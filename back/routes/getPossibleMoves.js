30c982246ebed8f96d3f2fc225ccc5d5f5dd1565
Adrien Mareel <56940107+AdriMareel@users.noreply.github.com>
2023-03-24 14:48:32 +0100

    FIX

--- back/routes/getPossibleMoves.js
+++ back/routes/getPossibleMoves.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
     app.get('/getPossibleMoves', (req, res) => {
-        const {x, y} = req.body.coordinates;
+        const {x, y} = req.body;
 
-        res.send(game.getPossibleMoves(req.body.coordinates));
+        res.send(game.getPossibleMoves(game.board[y][x]));
     });
 }
