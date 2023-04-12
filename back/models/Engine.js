47106574d5857a8517d80ca2e0b61dd94ded6d29
Adrien Mareel <mareel.adri@yahoo.com>
2023-04-12 11:11:49 +0200

    update min/max function Engine

--- back/models/Engine.js
+++ back/models/Engine.js
@@ -4,6 +4,9 @@ const Game = require("./Chess");
 module.exports = class Engine extends Game {
 	constructor() {
 		super();
+		let { score, moves } = this.minimax(this.board, 5, -Infinity, Infinity, true, []);
+		console.log("SCORE: ", score);
+		console.log("MOVES: ", moves);
 	}
 
 	update(board, turn) {
@@ -11,49 +14,76 @@ module.exports = class Engine extends Game {
 		this.turn = turn;
 	}
 
-	minimax(possibleMoves, depth, alpha, beta, maximizingPlayer) {
+	minimax(board, depth, alpha, beta, maximizingPlayer, movesPlayed) {
+		console.log("---------DEPTH" , depth, "---------");
 		if (depth === 0 || this.isGameOver(board)) {
-			return this.evaluateBoard(board);
+			return { score: this.evaluateBoard(board), moves: movesPlayed };
 		}
-
+	
+		const moves = this.getAllPossibleMoves(board, maximizingPlayer ? "white" : "black");
+		console.log("MOVES :", moves.length);
 		if (maximizingPlayer) {
-			maxEval = -Infinity;
-			for (let i = 0; i < possibleMoves.length; i++) {
-				const move = possibleMoves[i];
+			let maxEval = -Infinity;
+			let bestMoves = [];
+			for (const move of moves) {
 				this.simulateMove(move);
-				const evaluation = this.minimax(move, depth - 1, alpha, beta, false);
-				maxEval = Math.max(maxEval, evaluation);
-				alpha = Math.max(alpha, evaluation);
+				const { score, moves: nextMoves } = this.minimax(this.board, depth - 1, alpha, beta, false, [...movesPlayed, move]);
+				if (score > maxEval) {
+					maxEval = score;
+					bestMoves = [move, ...nextMoves];
+				}
+				alpha = Math.max(alpha, score);
 				if (beta <= alpha) {
 					break;
 				}
 			}
-		}	
-		
-		else {
-			minEval = Infinity;
-			for (let i = 0; i < possibleMoves.length; i++) {
-				const move = possibleMoves[i];
+			return { score: maxEval, moves: bestMoves };
+		} else {
+			let minEval = Infinity;
+			let bestMoves = [];
+			for (const move of moves) {
 				this.simulateMove(move);
-				const evaluation = this.minimax(move, depth - 1, alpha, beta, true);
-				minEval = Math.min(minEval, evaluation);
-				beta = Math.min(beta, evaluation);
+				const { score, moves: nextMoves } = this.minimax(this.board, depth - 1, alpha, beta, true, [...movesPlayed, move]);
+				if (score < minEval) {
+					minEval = score;
+					bestMoves = [move, ...nextMoves];
+				}
+				beta = Math.min(beta, score);
 				if (beta <= alpha) {
 					break;
 				}
 			}
+			return { score: minEval, moves: bestMoves };
 		}
 	}
 
-	simulateMove(move) {
-		const piece = this.board[move.y][move.x];
-		const pieceNext = this.board[move.yNext][move.xNext];
+	simulateMove(action) {
+		const piece = action.piece;
+		const move = action.move;
 
-		this.board[move.yNext][move.xNext] = piece;
-		this.board[move.y][move.x] = null;
+		console.log("SIMULATE MOVE");
+
+		let pieceX;
+		let pieceY;
+
+		for (let i = 0; i < this.board.length; i++) {
+			for (let j = 0; j < this.board[i].length; j++) {
+				if (this.board[i][j] === piece) {
+					pieceX = j;
+					pieceY = i;
+				}
+			}
+		}
+
+		this.board[move.y][move.x] = this.board[pieceY][pieceX];
+		this.board[pieceY][pieceX] = null;
+		this.board[move.y][move.x].moved = true;
+
+		this.changeTurn();
 	}
 
 	evaluateBoard(board) {
+		return 0;
 		let score = 0;
 
 		let pieceValues = {
@@ -91,7 +121,7 @@ module.exports = class Engine extends Game {
 			for (let x = 0; x < board[y].length; x++) {
 				const piece = board[y][x];
 				if (piece && piece.type === "pawn") {
-					const pawnScore = this.evaluatePawn(board, piece, x, y);
+					const pawnScore = this.evaluatePawn(board, piece, x, y);	
 					score += pawnScore * (piece.color === "white" ? 1 : -1);
 				}
 			}
@@ -161,5 +191,41 @@ module.exports = class Engine extends Game {
 		}
 
 		return score;
+	}
+
+	getAllPossibleMoves(board, color){
+		let possibleMoves = [];
+
+		for (let y = 0; y < board.length; y++) {
+			for (let x = 0; x < board[y].length; x++) {
+				const piece = board[y][x];
+				if (piece && piece.color === color) {
+					possibleMoves.push({piece : piece, move : this.getPossibleMoves(board, piece)});
+				}
+			}
+		}
+		possibleMoves.flat();
+		possibleMoves = possibleMoves.filter((action) => action.move.length > 0);
+		possibleMoves = possibleMoves.map(({ piece, move }) => ({ piece, move: move[0] }));
+
+		return possibleMoves;
+	}
+
+
+	isGameOver(board){
+		let whitePossibleMoves = this.getAllPossibleMoves(board, "white");
+		let blackPossibleMoves = this.getAllPossibleMoves(board, "black");
+
+		if (this.checkIfChecked(this.board, "white") && whitePossibleMoves.length === 0) {
+			console.log("------------GAME IS OVER------------");
+			return true;
+		}
+
+		if (this.checkIfChecked(this.board, "black") && blackPossibleMoves.length === 0) {
+			console.log("------------GAME IS OVER------------");
+			return true;
+		}
+
+		return false;
 	}
 }
